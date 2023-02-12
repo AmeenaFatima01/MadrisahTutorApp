80466cb3ef0b775c1b65792e0498287a8c89b1cb
AmeenaFatima01 <ameenafatima059@gmail.com>
2023-02-12 12:43:26 +0500

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button submit,view;
+    EditText date,name,ayat,sabaqi,manzil,salah;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        submit=findViewById(R.id.sbmitbtn);
+        view=findViewById(R.id.viewbtn);
+        date=findViewById(R.id.datefld);
+        name=findViewById(R.id.namefld);
+        ayat=findViewById(R.id.ayatfld);
+        sabaqi=findViewById(R.id.sabaqifld);
+        manzil=findViewById(R.id.manzilfld);
+        salah=findViewById(R.id.salahfld);
+
+    }
+}
