480faa5d178204ba8040b977a57bb978945daeb9
AmeenaFatima01 <ameenafatima059@gmail.com>
2023-02-12 12:50:26 +0500

    table with column variables added

--- app/src/main/java/com/example/myapplication/databaseHelper.java
+++ app/src/main/java/com/example/myapplication/databaseHelper.java
@@ -1,2 +1,34 @@
-package com.example.myapplication;public class databaseHelper {
+package com.example.myapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class databaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "student.db";
+    public static final String TABLE_NAME = "std";
+    public static final String COL_1 = "Date";
+    public static final String COL_2 = "Name";
+    public static final String COL_3 = "Ayat";
+    public static final String COL_4 = "Sabaqi";
+    public static final String COL_5 = "Manzil";
+    public static final String COL_6 = "Salah";
+
+
+    public databaseHelper(Context context)
+    {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
